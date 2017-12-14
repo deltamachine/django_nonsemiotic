f3941c03aa16a144db072e514d932e3b34922a7b
Anna Kondratjeva <an-an-kondratjeva@yandex.ru>
2017-12-14 18:04:51 +0300

    static fixed

--- lextyp/settings.py
+++ lextyp/settings.py
@@ -132,11 +132,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'nonsemiotic/static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
 
 LOCALE_PATHS = (
    os.path.join(BASE_DIR, 'locale'),
